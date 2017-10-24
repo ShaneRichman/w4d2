829680cfd513220cc6037c3c55bc519bb2d4d1fa
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-24 23:54:41 +0000

    simple search for person script

---
+++ look_people.js
@@ -0,0 +1,30 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const user_input = process.argv[2];
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people WHERE last_name = '${user_input}' OR first_name = '${user_input}';`, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+
+    console.log(result.rows.length);
+    console.log("Searching ...");
+    console.log(`Found ${result.rows.length} person(s) by the name ${user_input}:`);
+    for (let currentPerson in result.rows) {
+      console.log(`- ${result.rows[currentPerson].id}: ${result.rows[currentPerson].first_name} ${result.rows[currentPerson].last_name}, born ${result.rows[currentPerson].birthdate}`);
+    }
+    client.end();
+  });
+});
